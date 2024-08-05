2139049a8d6e1fdc19d238d099e64f69cc968377
Pritam <manoharipritam@gmail.com>
2024-08-05 20:32:26 +0530

    fix

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,17 +3,19 @@ import { FiMenu } from "react-icons/fi";
 
 const Navbar = () => {
   return (
-    <nav className='fixed w-full flex justify-between items-center px-20 2xl:px-60 py-6 border-b-2 border-b-gray-200 bg-white z-50'>
+    <nav className='fixed w-full border-b-2 border-b-gray-200 bg-white z-50'>
+      <div className=' w-full max-w-[1100px] mx-auto flex justify-between items-center py-6'>
         <img src="/images/logo.svg" alt="Logo" width={120} />
 
         <div className='navlinks flex gap-11 items-center text-md text-gray-500 '>
-            <a href="">SignUp</a>
-            <a href="">About</a>
-            <a href="">Products</a>
-            <a href="">Pricing</a>
-            <a href="">Support</a>
-            <FiMenu className=' text-2xl'/>
+          <a href="">SignUp</a>
+          <a href="">About</a>
+          <a href="">Products</a>
+          <a href="">Pricing</a>
+          <a href="">Support</a>
+          <FiMenu className=' text-2xl' />
         </div>
+      </div>
     </nav>
   )
 }
